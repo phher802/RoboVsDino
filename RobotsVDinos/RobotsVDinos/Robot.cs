cef477ac648c395ab0e690d0f5bbb5ef28fcf4e0
phher802 <pher024@gmail.com>
2020-11-09 13:58:09 -0600

    Add Robot Constructor

--- RobotsVDinos/RobotsVDinos/Robot.cs
+++ RobotsVDinos/RobotsVDinos/Robot.cs
@@ -9,11 +9,21 @@ namespace RobotsVDinos
     class Robot
     {
         // member variables (HAS A)
+        string robotName;
+        int robotHealth;
+        int robotPowerLevel;
+        string robotWeaponType;  //make into list?
+        int robotAttackPower;
+
 
         // constructor (SPAWNER)
-        public Robot()
+        public Robot(string name, string robotWeaponType)
         {
-
+           this.robotName = name;
+            robotHealth = 10;
+            robotPowerLevel = 10;
+           this.robotWeaponType = robotWeaponType;
+            robotAttackPower = 10;
         }
 
         // methods (can do)
